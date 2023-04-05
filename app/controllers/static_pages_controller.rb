579c4dde6af33e3dcb26e677c1aea0bd36c6d003
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-05 23:33:29 +0100

    feat(StaticPages): write logic for `#index` action

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def index
+    flickr = Flickr.new
+    if params[:user_id]
+      unless params[:user_id].empty?
+        @photos = flickr.photos.search(:user_id => params[:user_id])
+        @photos = @photos.first(10)
+      end
+    end
   end
 end
