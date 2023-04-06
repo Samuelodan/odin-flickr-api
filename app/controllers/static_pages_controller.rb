8e5a80cd33e3b15f3edd9f7422da598a995764f4
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-06 01:29:10 +0100

    refactor(StaticPages): use simpler syntax with hash argument.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,7 +3,7 @@ class StaticPagesController < ApplicationController
     flickr = Flickr.new
     if params[:user_id]
       unless params[:user_id].empty?
-        @photos = flickr.photos.search(:user_id => params[:user_id])
+        @photos = flickr.photos.search(user_id: params[:user_id])
         @photos = @photos.first(10)
       end
     end
