1e9cd312d1264d98ea582b54e5a06763765a82a8
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-06 02:02:49 +0100

    feat(StaticPages): use `per_page` option instead of modifying `@photos`

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,8 +4,7 @@ class StaticPagesController < ApplicationController
     if params[:user_id]
       begin
         unless params[:user_id].empty?
-          @photos = flickr.photos.search(user_id: params[:user_id])
-          @photos = @photos.first(10)
+          @photos = flickr.photos.search(user_id: params[:user_id], per_page: 10)
         end
       rescue Flickr::FailedResponse
         flash.now[:alert] = "User doesn't exist"
