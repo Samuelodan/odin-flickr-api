6c0adfa288ff89a3a0ca38a29db9207870aec3bc
Samuelodan <35481602+Samuelodan@users.noreply.github.com>
2023-04-06 01:58:48 +0100

    feat(StaticPages): rescue `Flickr::FailedResponse` error in `#index`

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,10 +2,16 @@ class StaticPagesController < ApplicationController
   def index
     flickr = Flickr.new
     if params[:user_id]
-      unless params[:user_id].empty?
-        @photos = flickr.photos.search(user_id: params[:user_id])
-        @photos = @photos.first(10)
+      begin
+        unless params[:user_id].empty?
+          @photos = flickr.photos.search(user_id: params[:user_id])
+          @photos = @photos.first(10)
+        end
+      rescue Flickr::FailedResponse
+        flash.now[:alert] = "User doesn't exist"
+        @photos = nil
       end
+
     end
   end
 end
